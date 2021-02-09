c9c69730879f54a4fdbd24446741f8e54791323b
Selmak <admztex@gmail.com>
2021-02-09 18:08:07 +0200

    PReducer

--- src/Redux/usersReduxer.js
+++ src/Redux/usersReduxer.js
@@ -2,13 +2,7 @@
 let initState = {
 
     users: [
-        {id: 1, name: 'Yuriy', status: 'online', location: {city: 'Kiyv', country: 'Ukraine'}, follow: true, url: 'https://medialeaks.ru/wp-content/uploads/2019/07/s1200-1-600x375.jpg' },
-        {id: 2, name: 'Hope', status: 'online', location: {city: 'Kiyv', country: 'Ukraine'}, follow: true, url: 'https://icdn.lenta.ru/images/2020/01/28/17/20200128170822958/square_320_9146846fb3b1bfae5672755bc1896214.jpg'  },
-        {id: 3, name: 'Mike', status: 'offline', location: {city: 'Kiyv', country: 'Ukraine'}, follow: true, url: 'https://wuzzup.ru/wp-content/uploads/2019/05/43054392_2250627378549261_137207783997206054_n.jpg'  },
-        {id: 4, name: 'Eve', status: 'online', location: {city: 'Kiyv', country: 'Ukraine'},follow: false, url: 'https://klike.net/uploads/posts/2019-07/1564314090_3.jpg'  },
     ]
-
-
 }
 
 const userReduxer = (state=initState, action) => {
@@ -24,10 +18,8 @@ const userReduxer = (state=initState, action) => {
                         return user;
                     })
                 }
-
             case "UNFOLLOW":
                     return {
-
             ...state,
                 users: state.users.map(user=> {
                 if (user.id === action.userId) {
@@ -36,17 +28,13 @@ const userReduxer = (state=initState, action) => {
                 return user;
             })
         }
-
             case "SET-USERS":
                 return {
                     ...state,
                     users: [ ...state.users, ...action.users ]
                 }
-
             default:
                 return state;
-
-
     }
 };
 
--- src/Users/Users.js
+++ src/Users/Users.js
@@ -1,15 +1,27 @@
 import styles from './Users.module.css';
+import axios from "axios";
+import DefaultUserLogo from "../img/user.png";
 
 
 
 const Users = (props) => {
 
+let getUsers =()=> {
 
+    if (props.users.length === 0) {
 
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items)
+            });
+    }
+}
     return(
 
-        <div className={styles.userspage}>
 
+
+    <div className={styles.userspage}>
+        <button onClick={getUsers}>Get</button>
             <div className={styles.users}>
 
                 {
@@ -17,18 +29,12 @@ const Users = (props) => {
                         <div>
 
 
-                              <img className={styles.avatar} src={item.url} width='80px'/>
+                              <img className={styles.avatar} src={item.photos.small != null ? item.photos.small: DefaultUserLogo} width='80px'/>
                               <div>{item.name}</div>
                             <div> {item.status}</div>
-                           <div>{item.location.city}, {item.location.country}</div>
+
                            <div> {item.follow ? <button onClick={ ()=> {props.unfollow(item.id)}}>Unfollow</button>:
                                <button onClick={ ()=> {props.follow(item.id)}}>Follow</button>     }</div>
-
-
-
-
-
-
 
                         </div>
 
